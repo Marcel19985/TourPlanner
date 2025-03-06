c277f45a3ed81147ba118b75ac8504adaa487234
Marcel Gössl <if23b093@technikum-wien.at>
2025-03-06 17:16:00 +0100

    Stylesheet eingebunden 2

--- src/main/java/org/example/tourplanner/TourPlannerApplication.java
+++ src/main/java/org/example/tourplanner/TourPlannerApplication.java
@@ -11,7 +11,7 @@ public class TourPlannerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TourPlannerApplication.class.getResource("/org/example/tourplanner/MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("TourPlanner");
         stage.setScene(scene);
         stage.show();
